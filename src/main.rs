485157e15cffe5b4559a69cb19552deaf6ce5b6e
scott-ados <coolrainy24@gmail.com>
2022-07-25 23:23:33 +0900

    token mint #1

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use {
+    solana_program_test::BanksClient,
+    solana_sdk::{
+        hash::Hash,
+        program_pack::Pack,
+        pubkey::Pubkey,
+        signature::{Keypair, Signer},
+        system_instruction,
+        transaction::Transaction,
+        transport::TransportError,
+    },
+    spl_token::{
+        id, instruction,
+        state::{Account, Mint},
+    },
+    solana_client::{nonblocking::rpc_client::RpcClient as AsyncRpcClient, rpc_client::RpcClient}
+};
+use std::time::Duration;
+use solana_sdk::commitment_config::CommitmentConfig;
+
+fn main() {
+    println!("Hello, world!");
+    //let args: Vec<String> = env::args().collect();
+
+
+    //let _cluster = &args[1];
+    //let _private_path = &args[2];
+    //let _amount = &args[3];
+
+    //println!("{}", _private_path);
+
+    //let contents = fs::read_to_string(_private_path).expect("파읽을 읽을 수 없습니다.");
+    //println!("읽은 파일 내용: \n {}", contents);
+
+    let conn = RpcClient::new_with_timeout_and_commitment(
+        "https://api.devnet.solana.com".to_string(),
+        Duration::from_secs(30),
+        CommitmentConfig::confirmed(),
+    );
+
+    let payer = Keypair::from_bytes(&[224,245,41,255,144,138,135,186,235,154,170,223,80,83,181,247,78,211,216,34,24,113,171,196,90,107,106,202,129,125,107,58,138,8,204,161,88,214,230,228,127,94,238,74,147,80,105,97,220,85,34,76,115,69,120,246,178,86,221,129,3,63,65,42]).unwrap();
+    let mint_account = Keypair::from_bytes(&[224,245,41,255,144,138,135,186,235,154,170,223,80,83,181,247,78,211,216,34,24,113,171,196,90,107,106,202,129,125,107,58,138,8,204,161,88,214,230,228,127,94,238,74,147,80,105,97,220,85,34,76,115,69,120,246,178,86,221,129,3,63,65,42]).unwrap();
+    let owner = Keypair::from_bytes(&[224,245,41,255,144,138,135,186,235,154,170,223,80,83,181,247,78,211,216,34,24,113,171,196,90,107,106,202,129,125,107,58,138,8,204,161,88,214,230,228,127,94,238,74,147,80,105,97,220,85,34,76,115,69,120,246,178,86,221,129,3,63,65,42]).unwrap();
+    println!("{}", &owner.pubkey());
+
+
+    let token_mint_inst = instruction::initialize_mint(
+        token_program,
+        &mint_account.pubkey(),
+        &mint_account.pubkey(),
+        None,
+        9,
+    )
+    .unwrap();
+
+    let blockhash = conn.get_latest_blockhash().unwrap();
+
+    // create mint transaction
+    let token_mint_tx = Transaction::new_signed_with_payer(
+        &[token_mint_inst],
+        Some(&payer.pubkey()),
+        &[&payer, &mint_account],
+        blockhash,
+    ).unwrap();
+
+    println!("Deposit done. Signature: {}", token_mint_tx);
+}
