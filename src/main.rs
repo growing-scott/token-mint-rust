874324e5b6f20a62eeae375da9fdc133aae78c09
scott-ados <coolrainy24@gmail.com>
2022-07-26 15:48:02 +0900

    token mint #5

--- src/main.rs
+++ src/main.rs
@@ -57,16 +57,16 @@ fn main() {
 
     // Account Balance Check(Payer)
     let account = conn.get_account(&payer.pubkey()).unwrap();
-    println!("account: {}", account.lamports);
+    println!("account balance(sol): {}", account.lamports);
     if account.lamports < 2000000000 {
         panic!("Not enough balance");
     }
-
 
     // Mint Token
     let mint_rent = conn.get_minimum_balance_for_rent_exemption(Mint::LEN).unwrap();
 
     let mint_account = Keypair::new();
+    println!("token address: {}", mint_account.pubkey().to_string());
 
     let token_mint_account_ix = solana_program::system_instruction::create_account(
         &payer.pubkey(),
@@ -120,8 +120,7 @@ fn main() {
         &owner.pubkey(),
         &[],
         _amount.unwrap().clone(),
-    )
-        .unwrap();
+    ).unwrap();
 
     let mint_to_tx = Transaction::new_signed_with_payer(
         &[mint_to_ix],
@@ -131,69 +130,4 @@ fn main() {
     );
     let mint_to_signature = conn.send_and_confirm_transaction(&mint_to_tx).unwrap();
     println!("Mint to. Signature: {}", mint_to_signature);
-
-
-
-
-
-
-
-    // Create account that can hold the newly minted tokens
-
-    /*
-    let account_rent = conn.get_minimum_balance_for_rent_exemption(Account::LEN).unwrap();
-    let token_account = Keypair::new();
-    let new_token_account_ix = system_instruction::create_account(
-        &payer.pubkey(),
-        &token_account.pubkey(),
-        account_rent,
-        Account::LEN as u64,
-        token_program,
-    );
-
-    let my_account = Keypair::new();
-
-    let initialize_account_ix = instruction::initialize_account(
-        &spl_associated_token_account::id(),
-        &token_account.pubkey(),
-        &mint_account.pubkey(),
-        &my_account.pubkey(),
-    )
-        .unwrap();
-
-    let create_new_token_account_tx = Transaction::new_signed_with_payer(
-        &[new_token_account_ix, initialize_account_ix],
-        Some(&payer.pubkey()),
-        &[&payer, &token_account],
-        recent_blockhash,
-    );
-    let signature = conn.send_and_confirm_transaction(&create_new_token_account_tx).unwrap();
-    println!("create_new_token_account. Signature: {}", signature);
-     */
-    //let mint_account = Pubkey::new("2MpTrG9Wes5Xh3cpf4JyCoGo1gnHAYbcQfnRFLfTxDTN".as_bytes());
-
-    //let mint_account = Pubkey::from_str("2MpTrG9Wes5Xh3cpf4JyCoGo1gnHAYbcQfnRFLfTxDTN").unwrap();
-    //let associated_program_id = Pubkey::from_str("2MpTrG9Wes5Xh3cpf4JyCoGo1gnHAYbcQfnRFLfTxDTN").unwrap();
-
-
-    /*
-    let associated_token_account_ix= spl_associated_token_account::instruction::create_associated_token_account(&payer.pubkey(), &owner.pubkey(), &mint_account);
-
-    let create_new_token_account_tx = Transaction::new_signed_with_payer(
-        &[associated_token_account_ix],
-        Some(&payer.pubkey()),
-        &[&payer],
-        recent_blockhash,
-    );
-
-    let signature = conn.send_and_confirm_transaction(&create_new_token_account_tx).unwrap();
-    println!("create_new_token_account_tx signature. {}", signature);
-    */
-
-
-
-
-
-
-
 }
